a0f96426824808f167f24873a0cf948902dcc5a0
kylm53 <skyping1987@gmail.com>
2016-07-29 23:23:46 +0800

    add return status field

--- app/src/main/java/com/kylm/weather/commons/APIs.java
+++ app/src/main/java/com/kylm/weather/commons/APIs.java
@@ -11,9 +11,44 @@ public final class APIs {
     public static final String KEY = "3c85fe013f9f42049999ca4fe81ac854";
     public static final String BASE_URL = "https://api.heweather.com/x3/";
 
-    public static final String SEARCH_TYPE_ALL_CHINA = "allchina";
-    public static final String SEARCH_TYPE_HOT_WORLD = "hotworld";
-    public static final String SEARCH_TYPE_ALL_WORLD = "allworld";
+    //查询城市代码类型
+    public static final String TYPE_ALL_CHINA = "allchina";
+    public static final String TYPE_HOT_WORLD = "hotworld";
+    public static final String TYPE_ALL_WORLD = "allworld";
+
+    //查询天气代码类型
+    public static final String TYPE_CONDITION_ALL_CONDITONS = "allcond";
+
+
+    /**
+     * 接口正常
+     */
+    public static final String STATUS_OK = "ok";
+
+    /**
+     * 错误的用户 key
+     */
+    public static final String STATUS_INVALID_KEY = "invalid key";
+
+    /**
+     * 未知城市
+     */
+    public static final String STATUS_UNKNOWN_CITY = "unknown city";
+
+    /**
+     * 超过访问次数
+     */
+    public static final String STATUS_NO_MORE_REQUESTS = "no more requests";
+
+    /**
+     * 没有访问权限
+     */
+    public static final String STATUS_PERMISSION_DENIED = "permission denied";
+
+    /**
+     * 服务无响应或超时
+     */
+    public static final String STATUS_ANR = "anr";
 
 
     private static Retrofit retrofit = new Retrofit.Builder()
@@ -23,4 +58,5 @@ public final class APIs {
             .build();
 
     public static WeatherService service = retrofit.create(WeatherService.class);
+
 }
